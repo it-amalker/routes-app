dfe183aee45f10f5bc36bf036419c2a2fa39477a
amalker <it.amalker@yandex.ru>
2020-06-10 15:50:31 +0300

    add routes container

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -6,9 +6,12 @@ const App: React.FC = () => {
   const [markers, setMarker] = useState<MarkerType[]>([]);
 
   return (
-    <div>
-      <Map markers={markers} setMarker={setMarker} />
-    </div>
+    <>
+      <div className="routes-container">Some data</div>
+      <div className="map-container">
+        <Map markers={markers} setMarker={setMarker} />
+      </div>
+    </>
   );
 };
 
