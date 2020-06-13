c4e31df36d79238adc238233945ac08972f04840
amalker <it.amalker@yandex.ru>
2020-06-13 16:28:53 +0300

    add new html elements, add remove markers functionally

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -6,10 +6,21 @@ import { MarkerType } from '../types/marker';
 const App: React.FC = () => {
   const [markers, setMarker] = useState<MarkerType[]>([]);
 
+  const removeMarker = (id: string) => () => {
+    const updatedMarkers = markers.filter((m) => m.id !== id);
+    setMarker(() => updatedMarkers);
+  };
+
   return (
     <>
       <div className="routes-container">
-        <RoutesUI markers={markers} />
+        <h1 className="app-title">Routes app</h1>
+        <p className="routes-info">Move blocks to adjust the route:</p>
+        <RoutesUI
+          markers={markers}
+          setMarker={setMarker}
+          removeMarker={removeMarker}
+        />
       </div>
       <div className="map-container">
         <Map markers={markers} setMarker={setMarker} />
