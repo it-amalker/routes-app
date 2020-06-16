34154c2111816a8f201d1476d4b0c19094b7b079
amalker <it.amalker@yandex.ru>
2020-06-16 17:45:30 +0300

    add travel mode props type, add routesNotFound function types in Maps and Directions props types

--- src/types/props.ts
+++ src/types/props.ts
@@ -1,21 +1,33 @@
 import { MarkerType } from './marker';
+import { TravelModeType } from './types';
 
 export type SearchProps = {
   navigateTo: (lat: number, lng: number) => void;
 };
 
 type PrevMarkersState = (markers: MarkerType[]) => MarkerType[];
+type PrevRouteNotFoundState = (prev: boolean) => boolean;
 
 export type MapProps = {
   markers: MarkerType[];
   setMarker: (f: PrevMarkersState) => void;
+  setRouteNotFound: (f: PrevRouteNotFoundState) => void;
 };
 
 export type DirectionsProps = {
   places: MarkerType[];
   travelMode: google.maps.TravelMode;
+  setRouteNotFound: (f: PrevRouteNotFoundState) => void;
 };
 
 export type RoutesUIProps = {
+  markers: MarkerType[];
+  setMarker: (f: PrevMarkersState) => void;
   removeMarker: (id: string) => () => void;
-} & MapProps;
+};
+
+type PrevTravelModeState = (travelMode: TravelModeType) => TravelModeType;
+
+export type TravelModeProps = {
+  setTravelMode: (f: PrevTravelModeState) => void;
+};
