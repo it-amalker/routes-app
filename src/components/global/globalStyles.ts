1189aba702597c18a3e2d79577222e301a8cc9c8
amalker <it.amalker@yandex.ru>
2020-07-20 19:16:30 +0300

    add global styles

---
+++ src/components/global/globalStyles.ts
@@ -0,0 +1,12 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    box-sizing: border-box;
+  
+    font-family: 'Open Sans', sans-serif;
+  }
+`;
+
+export default GlobalStyle;
---
+++ src/components/global/theme.ts
@@ -0,0 +1,22 @@
+const theme = {
+  colors: {
+    main: '#3878c7',
+    secondary: '#fafafa',
+    // mobile: '#6aa121',
+    // tablet: '#327ac6',
+  },
+  // fontSizes: {
+  //   large: '4.15rem',
+  //   big: '2.25rem',
+  //   medium: '1.25rem',
+  //   small: '1.1rem',
+  //   smallest: '0.9rem',
+  // },
+  // mediaQueries: {
+  //   desktop: '(min-width: 1024px)',
+  //   tablet: '(min-width: 640px) and (max-width: 1023px)',
+  //   mobile: '(max-width: 639px)',
+  // },
+};
+
+export default theme;
