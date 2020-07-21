c81ef7e42aa591274371eed2b9a2f8a027beda88
amalker <it.amalker@yandex.ru>
2020-07-21 13:50:06 +0300

    change app import

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { ThemeProvider } from 'styled-components';
 
-import App from './components/App';
+import App from './components/app';
 import GlobalStyle from './components/global/globalStyles';
 import Theme from './components/global/theme';
 
