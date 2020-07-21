9e90a539a411cb916773c2fe88e48bc308adce95
amalker <it.amalker@yandex.ru>
2020-07-21 13:47:50 +0300

    change app directories structure

---
+++ src/components/app/App.tsx
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+import Map from '../map';
+import RoutesUI from '../routesUI';
+import { MarkerType } from '../../types/marker';
+import Layout from '../layout';
+
+const App: React.FC = () => {
+  const [markers, setMarker] = useState<MarkerType[]>([]);
+  const [routeNotFound, setRouteNotFound] = useState(false);
+
+  const removeMarker = (id: string) => () => {
+    const updatedMarkers = markers.filter((m) => m.id !== id);
+    setMarker(() => updatedMarkers);
+  };
+
+  return (
+    <Layout>
+      <RoutesUI
+        markers={markers}
+        setMarker={setMarker}
+        removeMarker={removeMarker}
+        routeNotFound={routeNotFound}
+      />
+      <Map
+        markers={markers}
+        setMarker={setMarker}
+        setRouteNotFound={setRouteNotFound}
+      />
+    </Layout>
+  );
+};
+
+export default App;
