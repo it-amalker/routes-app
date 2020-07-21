8264644be1cdc7da8dc442ac39ef5703c636133c
amalker <it.amalker@yandex.ru>
2020-07-21 13:36:33 +0300

    remove hidden title

--- src/components/layout/Layout.styles.ts
+++ src/components/layout/Layout.styles.ts
@@ -1,6 +1,6 @@
 import styled from 'styled-components';
 
-export const Main = styled.div`
+const Main = styled.div`
   display: flex;
   width: 100vw;
   height: 100vh;
@@ -8,13 +8,4 @@ export const Main = styled.div`
   color: ${({ theme }) => theme.colors.secondary};
 `;
 
-export const Title = styled.h1`
-  position: absolute;
-  width: 1px;
-  height: 1px;
-  margin: -1px;
-  padding: 0;
-  border: 0;
-  white-space: nowrap;
-  overflow: hidden;
-`;
+export default Main;
--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,16 +1,11 @@
 import React, { ReactNode } from 'react';
 
-import { Main, Title } from './Layout.styles';
+import Main from './Layout.styles';
 
 type LayoutProps = {
   children?: ReactNode;
 };
 
-const Layout: React.FC<LayoutProps> = ({ children }) => (
-  <Main>
-    <Title>Routes App</Title>
-    {children}
-  </Main>
-);
+const Layout: React.FC<LayoutProps> = ({ children }) => <Main>{children}</Main>;
 
 export default Layout;
