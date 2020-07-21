92ca06c86707ba9b23ce4a9dcfd8f48cbb04ba44
amalker <it.amalker@yandex.ru>
2020-07-21 13:37:45 +0300

    remove styles import

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { ThemeProvider } from 'styled-components';
-import '@reach/combobox/styles.css';
 
 import App from './components/App';
 import GlobalStyle from './components/global/globalStyles';
 import Theme from './components/global/theme';
 
-import './styles/style.css';
+import '@reach/combobox/styles.css';
 
 ReactDOM.render(
   <ThemeProvider theme={Theme}>
